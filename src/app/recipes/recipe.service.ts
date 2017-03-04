75ec7308fce746f681d320fb5bc3d301f2343082
ZGrauer <zgrauer@wgu.edu>
2017-03-04 11:41:00 -0600

    feat: Show recipe ingredients when selected.  Impliment 'To shopping list" button.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -4,8 +4,11 @@ import {Ingredient} from '../ingredient';
 
 @Injectable()
 export class RecipeService {
-  private recipes: Recipe[] = [new Recipe('Pruttled', 'Best breakfast food ever!', 'https://dannwoellertthefoodetymologist.files.wordpress.com/2014/10/images.jpg', [new Ingredient('Pork', 2), new Ingredient('Beef', 1), new Ingredient('Oats', 1)]),
-    new Recipe('Sauerkraut Balls', 'Meatball-sized fritter containing sauerkraut and ham', 'http://img.sndimg.com/food/image/upload/h_420,w_560,c_fit/v1/img/recipes/13/70/0/picqGwJLm.jpg', [new Ingredient('Ham', 3), new Ingredient('Sauerkraut', 1)])];
+  private recipes: Recipe[] = [new Recipe('Pruttles', 'Best breakfast food ever!  Shown here with eggs and toast.', 'https://dannwoellertthefoodetymologist.files.wordpress.com/2014/10/images.jpg', [new Ingredient('Pork', 2, 'lbs'),
+  new Ingredient('Beef', 1, 'lbs'),
+  new Ingredient('Oats', 1, 'cup')]),
+    new Recipe('Sauerkraut Balls', 'Meatball-sized fritter containing sauerkraut and ham', 'http://img.sndimg.com/food/image/upload/h_420,w_560,c_fit/v1/img/recipes/13/70/0/picqGwJLm.jpg', [new Ingredient('Ham', 3, 'lbs'),
+    new Ingredient('Sauerkraut', 1, 'cup')])];
   constructor() { }
 
   getRecipes() {
