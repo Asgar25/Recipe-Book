8cc6db480e8b47f7b81ad46c1c9e08409b8d67b1
ZGrauer <zgrauer@wgu.edu>
2017-03-06 17:22:25 -0600

    feat: Implement Recipe’s Edit functionality/form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,15 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FormArray, FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs/Rx';
+
+import { RecipeService } from '../recipe.service';
+import { Recipe } from '../recipe';
 
 @Component({
   selector: 'rb-recipe-edit',
   templateUrl: './recipe-edit.component.html',
   styleUrls: ['./recipe-edit.component.css']
 })
-export class RecipeEditComponent implements OnInit {
+export class RecipeEditComponent implements OnInit, OnDestroy {
+  recipeForm: FormGroup;
+  private subscription: Subscription;
+  private recipeIndex: number;
+  private recipe: Recipe;
+  private isNew: boolean = true;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    // Subscribe to changes in route params.  Use to get current recipe ID
+    this.subscription = this.route.params.subscribe(
+      (params: any) => {
+        if (params.hasOwnProperty('id')) {  // is there an ID in the Route params?
+          this.isNew = false;
+          this.recipeIndex = +params['id'];  // + converts string id to a number for index
+          this.recipe = this.recipeService.getRecipe(this.recipeIndex);
+        } else {
+          this.isNew = true;
+          this.recipe = null;
+        }
+        this.initForm();
+      }
+    )
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  private initForm() {
+    let recipeName = '';
+    let recipeImageUrl = '';
+    let recipeDesc = '';
+    let recipeIngredients: FormArray = new FormArray([]);
+
+    if (!this.isNew) {
+        console.dir(this.recipe);
+      for (let i = 0; i < this.recipe.ingredients.length; i++) {
+        recipeIngredients.push(
+          new FormGroup({
+            name: new FormControl(this.recipe.ingredients[i].name, Validators.required),
+            amount: new FormControl(this.recipe.ingredients[i].amount, [Validators.required, Validators.pattern('\\d+')]),
+            units: new FormControl(this.recipe.ingredients[i].units, Validators.required)
+          })
+        );
+      }
+      recipeName = this.recipe.name;
+      recipeDesc = this.recipe.description;
+      recipeImageUrl = this.recipe.imagePath;
+    }
+    this.recipeForm = this.formBuilder.group({
+      name: [recipeName, Validators.required],
+      description: [recipeDesc, Validators.required],
+      imageUrl: [recipeImageUrl, Validators.required],
+      ingredients: recipeIngredients
+    });
   }
 
 }
